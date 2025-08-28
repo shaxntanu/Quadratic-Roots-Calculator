6d0febf38c74aa0d83c4a77a0d7cd3f06e2b7a2e
shantanu <i.am.shantanu07@gmail.com>
2025-08-28 15:25:22 +0530

    Update Quad Root Calc..c

---
+++ Quad Root Calc..c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+    printf("Welcome to the Quadratic Roots Calculator\n");
+    int a, b, c;
+    int d;
+    double root1, root2;
+    printf("Enter coeff. a = ");
+    scanf("%d", &a);
+    printf("\nEnter coeff. b = ");
+    scanf("%d", &b);
+    printf("\nEnter coeff. c = ");
+    scanf("%d", &c);
+
+    d = b*b - 4*a*c;
+    if(d>=0){
+    root1 = (-b + sqrt(d)) / (2.0 * a);
+    root2 = (-b - sqrt(d)) / (2.0 * a);
+
+    printf("\nRoot1 = %lf\n", root1);
+    printf("Root2 = %lf\n", root2);
+    }
+    else{
+        printf("\nImaginary Roots\n");
+    }
+printf("\nMade by Shantanu");
+    return 0;
+}
